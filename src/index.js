1740beb931b21ef5dbb90dc94eb2f539ec6720d5
muhammadiqbalid83 <121326593+muhammadiqbalid83@users.noreply.github.com>
2023-01-12 16:39:00 +0700

    error page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root.jsx';
+import ErrorPage from './error-page.jsx';
 import './style.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
